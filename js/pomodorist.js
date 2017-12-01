57bc5da32a58f5e095bd66fe8446e3d435816f82
nemanja-s <s.nemanja@protonmail.ch>
2017-11-30 21:12:26 -0800

    add timer in js

--- js/pomodorist.js
+++ js/pomodorist.js
@@ -1,1 +1,23 @@
+var workInSeconds = 15;
+var timer = document.getElementById('timer');
 
+function isNegative(num) {
+    if (isNaN(num)) {
+        return false;
+    }
+    Math.min(num, 0) != 0
+}
+
+function countdown() {
+    workInSeconds--;
+    var minutes = Math.floor(workInSeconds / 60);
+    var seconds = Math.floor(workInSeconds % 60);
+    timer.innerHTML = "Work time is: " + minutes + "m " + seconds + "s";
+
+    if (isNegative(workInSeconds)) {
+        clearInterval(interval);
+        timer.innerHTML = "Your work time is over, take a break"
+    }
+}
+
+var interval = setInterval(countdown, 1000);
